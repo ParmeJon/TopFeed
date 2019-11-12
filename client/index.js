6a0a20739211e2cb31dded7e2fcc3a05c1d12743
github username <jonathansychan@gmail.com>
2019-11-12 16:32:07 -0500

    Edits frontend js

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get("myParam");
+console.log(myParam)
+fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink,like_count&access_token=${myParam.access_token}`)
+.then((res) => {
+    console.log(res)
+})
 document.getElementById('authorize-btn')
 document.addEventListener('click', (e) => {
-    fetch('/auth')
-    .then((res) => {
-        console.log(res)
-    })
+    console.log("clickable")
 })
