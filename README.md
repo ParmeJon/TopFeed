21c4b86294281d9347ebf5f350edf968ec4bc2bd
github username <jonathansychan@gmail.com>
2019-11-12 17:50:55 -0500

    Changes readme and some configurations

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 To provide a user interface of your most liked media content on instagram.
 
+## Goals
+
+- Implement Instagram Oauth
+- Express serves up react app on "/"
+
+## Issues
+
+- CORS issue when requesting routes with access_token sent from express server as a query parameter 
+- Sending access_token to frontend from express backend routes with all the callback and redirecting involved. 
+
--- topfeed-client/src/App.js
+++ topfeed-client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function authorize(e) {
+    e.preventDefault();
+    console.log('the link was clicked')
+  }
   return (
     <div className="App">
       <h1>TOPFEED</h1>
-      <a href="/auth">Authorize with Instagram</a>
+      <button onClick={authorize}>Authorize with Instagram</button>
     </div>
   );
 }
