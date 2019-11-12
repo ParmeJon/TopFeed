5ed944a9cd405d642a296afd17c0f54e75146855
github username <jonathansychan@gmail.com>
2019-11-12 16:42:54 -0500

    Attempts to bypass cors issue

--- client/index.js
+++ client/index.js
@@ -1,13 +1,16 @@
 const urlParams = new URLSearchParams(window.location.search);
 const access_token = urlParams.get("access_token");
 console.log(access_token)
-if (access_token) {
-    fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink,like_count&access_token=${access_token}`)
-    .then((res) => {
-        console.log(res)
+if (access_token, {}) {
+    fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink,like_count&access_token=${access_token}`, {
+        credentials: 'include'
+    })
+    .then(res => res.json()) 
+    .then(json => {
+        console.log(json)
     })
 }
-document.getElementById('authorize-btn')
-document.addEventListener('click', (e) => {
+let authorizeBtn = document.getElementById('authorize-btn')
+authorizeBtn.addEventListener('click', (e) => {
     console.log("clickable")
 })
